1859c6851bb32abac39338b4280143e2b71cd8c4
young <85962238+dardardardardar@users.noreply.github.com>
2024-07-26 22:59:24 +0900

    가위바위보 게임
    선택하면 백그라운드 색 바꿔주고
    컴퓨터도 랜덤으로 골라져서
    결과창 출력
    (결과에 따른 배경색도 변경됨)

---
+++ RockScissorsPaperGame/RockScissorsPaperGame/RockScissorsPaperGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockScissorsPaperGameApp.swift
+//  RockScissorsPaperGame
+//
+//  Created by 이다영 on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct RockScissorsPaperGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameView()
+        }
+    }
+}
